8f2d66df0a44c73cb7423b16314691908efcd43d
tranthienminh135 <67372816+tranthienminh135@users.noreply.github.com>
2022-08-09 15:30:14 +0700

    phuong up login

---
+++ coffee-store/src/main/java/com/coffee/service/jwt/JwtUserDetailsService.java
@@ -0,0 +1,59 @@
+package com.coffee.service.jwt;
+
+import com.coffee.model.account.AppUser;
+import com.coffee.model.account.UserRole;
+import com.coffee.repository.account.IAppUserRepository;
+import com.coffee.repository.account.IUserRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IAppUserRepository IAppUserRepository;
+
+    @Autowired
+    private IUserRoleRepository IUserRoleRepository;
+
+    /**
+     *
+     * @creator PhuongTD
+     * @param userName
+     * @return username, password, roles list
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        AppUser appUser = this.IAppUserRepository.getAppUserByUsername(userName);
+
+        if (appUser == null) {
+            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+        }
+
+        // [ROLE_USER, ROLE_ADMIN,..]
+        List<UserRole> userRoles = this.IUserRoleRepository.findAllByAppUser(appUser);
+
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        if (userRoles != null) {
+            for (UserRole userRole : userRoles) {
+                // ROLE_USER, ROLE_ADMIN,..
+                GrantedAuthority authority = new SimpleGrantedAuthority(userRole.getAppRole().getRoleName());
+                grantList.add(authority);
+            }
+        }
+
+        return new User(appUser.getUserName(), //
+                appUser.getPassword(), grantList);
+    }
+}
