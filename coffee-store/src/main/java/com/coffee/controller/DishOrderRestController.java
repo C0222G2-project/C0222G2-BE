6227eee206716d78cc5d1861616f4c6d0a66f24c
BaoAsus <your_email_address@example.com>
2022-08-09 16:31:03 +0700

    up

--- coffee-store/src/main/java/com/coffee/controller/DishOrderRestController.java
+++ coffee-store/src/main/java/com/coffee/controller/DishOrderRestController.java
@@ -1,6 +1,8 @@
 package com.coffee.controller;
 
+import com.coffee.model.dish.Dish;
 import com.coffee.model.dish_order.DishOrder;
+import com.coffee.repository.IDishRepository;
 import com.coffee.service.IDishOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,14 +18,21 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/dish-order")
 public class DishOrderRestController {
+    /**
+     * Created by: BaoTQ
+     * Date create: 09/08/2022
+     * function: get 5 dish most order
+     */
+
     @Autowired
     private IDishOrderService iDishOrderService;
 
 
-    //RestController Method Get List Most Order By BaoTQ
+
     @GetMapping("/most-order")
     public ResponseEntity<List<DishOrder>> getMostOrder(){
         List<DishOrder> mostOrderList = iDishOrderService.get5DishMostOrder();
         return new ResponseEntity<>(mostOrderList, HttpStatus.OK);
     }
+
 }
--- coffee-store/src/main/java/com/coffee/repository/IDishOrderRepository.java
+++ coffee-store/src/main/java/com/coffee/repository/IDishOrderRepository.java
@@ -9,10 +9,15 @@ import java.util.List;
 
 @Repository
 public interface IDishOrderRepository extends JpaRepository<DishOrder, Integer> {
+    /**
+     * Created by: BaoTQ
+     * Date create: 09/08/2022
+     * function: get 5 dish most order in database
+     */
 
-    //Method Get List Most Order By BaoTQ
     @Query(value = " SELECT dish_id FROM (SELECT dish_order.dish_id, sum(quantity) as time_order FROM dish_order " +
             " GROUP BY  dish_order.dish_id ORDER BY time_order DESC LIMIT 5) as test_table;",nativeQuery = true)
     List<DishOrder> get5DishMostOrder();
 
+    
 }
