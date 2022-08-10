d883a3187718bb6dfb7ee5fb966799b6b8737d70
unknown <you@example.com>
2022-08-10 15:47:49 +0700

    ms

--- coffee-store/src/test/java/com/coffee/controller/AccountEmployeeRestController_getInfoAccountEmployee.java
+++ coffee-store/src/test/java/com/coffee/controller/AccountEmployeeRestController_getInfoAccountEmployee.java
@@ -16,18 +16,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AccountEmployeeRestController_getInfoAccountEmployee {
     @Autowired
     private MockMvc mockMvc;
-
+    /**
+     * @param "id" = "null"
+     * @status 4xx
+     * @return Trả về nội dung lỗi FieldError ở REST
+     * @creator ThongNV
+     * Date 10/08/2022
+     */
     @Test
     public void  getInfoAccountEmployee_id_1() throws Exception {
         this.mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/account/accountEmployee/null")).andDo(print()).andExpect(status().is4xxClientError());
     }
+    /**
+     * @param "id" = "rỗng"
+     * @status 4xx
+     * @return Trả về nội dung lỗi FieldError ở REST
+     * @creator ThongNV
+     * Date 10/08/2022
+     */
     @Test
     public void getInfoAccountEmployee_id_2() throws Exception {
         this.mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/account/accountEmployee/")
         ).andDo(print()).andExpect(status().is4xxClientError());
     }
+    /**
+     * @param "id" = không tồn tại trong DB
+     * @status 4xx
+     * @return Trả về nội dung lỗi FieldError ở REST
+     * @creator ThongNV
+     * Date 10/08/2022
+     */
     @Test
     public void getInfoAccountEmployee_id_3() throws Exception {
         this.mockMvc.perform(
@@ -35,6 +55,13 @@ public class AccountEmployeeRestController_getInfoAccountEmployee {
         ).andDo(print()).andExpect(status().is4xxClientError());
 
     }
+    /**
+     * @param "id" = "Tồn tại trong DB
+     * @status 2xx
+     * @return"Dữ liệu là JSON, chứa đầy đủ các thuộc tính ở object, đảm bảo 2 yếu tố: Số lượng thuộc tính + Dữ liệu thuộc tính."
+     * @creator ThongNV
+     * Date 10/08/2022
+     */
     @Test
     public void getInfoAccountEmployee_id_4() throws Exception {
 
