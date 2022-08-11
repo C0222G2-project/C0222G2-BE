e454060622e2e6ff04ea006a9f5d1ac0aac08baf
BaoAsus <your_email_address@example.com>
2022-08-11 14:15:47 +0700

    baotq update-fix-test

--- coffee-store/src/test/java/com/coffee/controller/DishOrderRestController_getListDishMostOrder.java
+++ coffee-store/src/test/java/com/coffee/controller/DishOrderRestController_getListDishMostOrder.java
@@ -37,10 +37,10 @@ public class DishOrderRestController_getListDishMostOrder {
     /**
      * Created by: BaoTQ
      * Date create: 10/08/2022
-     * Function: test list size > 0
+     * Function: test list size > 0, and check first dish in list
      */
     @Test
-    public void getListDishMostOrder_6() throws Exception {
+    public void getListDishMostOrderFirst_6() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/dish-order/most-order", new Object[0]))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
@@ -50,6 +50,25 @@ public class DishOrderRestController_getListDishMostOrder {
                 .andExpect(MockMvcResultMatchers.jsonPath("[0].image", new Object[0]).value("url4"))
                 .andExpect(MockMvcResultMatchers.jsonPath("[0].timeOrder", new Object[0]).value(17))
                 .andExpect(MockMvcResultMatchers.jsonPath("[0].dishId", new Object[0]).value(4));
+    }
+
+
+    /**
+     * Created by: BaoTQ
+     * Date create: 10/08/2022
+     * Function: test list size > 0, and check last dish in list
+     */
+    @Test
+    public void getListDishMostOrderLast_6() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/dish-order/most-order", new Object[0]))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(5)))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].name", new Object[0]).value("Sữa tươi"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].price", new Object[0]).value(10000))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].image", new Object[0]).value("url1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].timeOrder", new Object[0]).value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].dishId", new Object[0]).value(1));
     }
 
 
--- coffee-store/src/test/java/com/coffee/controller/DishOrderRestController_getListNewest.java
+++ coffee-store/src/test/java/com/coffee/controller/DishOrderRestController_getListNewest.java
@@ -37,20 +37,39 @@ public class DishOrderRestController_getListNewest {
 
     /**
      * Created by: BaoTQ
-     * Date create: 09/08/2022
-     * Function: test list size > 0
+     * Date create: 10/08/2022
+     * Function: test list size > 0, and check first dish in list
      */
     @Test
-    public void getListDishMostOrder_6() throws Exception {
+    public void getListDishMostOrderFirst_6() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/dish-order/newest", new Object[0]))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(5)))
-                .andExpect(MockMvcResultMatchers.jsonPath("[2].name", new Object[0]).value("Soda dâu"))
-                .andExpect(MockMvcResultMatchers.jsonPath("[2].id", new Object[0]).value(3))
-                .andExpect(MockMvcResultMatchers.jsonPath("[2].price", new Object[0]).value(20000))
-                .andExpect(MockMvcResultMatchers.jsonPath("[2].image", new Object[0]).value("url3"))
-                .andExpect(MockMvcResultMatchers.jsonPath("[2].dateCreate", new Object[0]).value("2022-04-11"));
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].name", new Object[0]).value("Cà phê trứng"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].id", new Object[0]).value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].price", new Object[0]).value(30000))
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].image", new Object[0]).value("url5"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].dateCreate", new Object[0]).value("2022-06-11"));
+    }
+
+
+    /**
+     * Created by: BaoTQ
+     * Date create: 10/08/2022
+     * Function: test list size > 0, and check last dish in list
+     */
+    @Test
+    public void getListDishMostOrderLast_6() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/dish-order/newest", new Object[0]))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(5)))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].name", new Object[0]).value("Sữa tươi"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].id", new Object[0]).value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].price", new Object[0]).value(10000))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].image", new Object[0]).value("url1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[4].dateCreate", new Object[0]).value("2022-02-11"));
     }
 
 }
