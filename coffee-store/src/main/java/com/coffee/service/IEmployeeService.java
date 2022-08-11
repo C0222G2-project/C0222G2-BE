717a8b39fdbdafb7151340083f5c43e365976f94
tunhutuyen <conlocdn@gmail.com>
2022-08-11 15:40:29 +0700

    update again

--- coffee-store/src/main/java/com/coffee/service/IEmployeeService.java
+++ coffee-store/src/main/java/com/coffee/service/IEmployeeService.java
@@ -3,7 +3,6 @@ package com.coffee.service;
 
 
 import com.coffee.dto.employe.IEmployeeDTO;
-import com.coffee.model.employee.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -33,35 +32,6 @@ public interface IEmployeeService {
      * @param id
      */
     void deleteEmployeeById(Integer id);
-
-
-    /**
-     * @creator TaiLV
-     * Date 09/08/2022
-     * @param employee
-     * if employee null : Create new employee
-     * @return  create Employee success
-     */
-    Employee saveEmployee(Employee employee);
-
-    /**
-     * @creator TaiLV
-     * Date 09/08/2022
-     * @param id
-     * if id null : Bad request
-     * @return  object Employee
-     */
-    Employee findById(Integer id);
-
-    /**
-     * @creator TaiLV
-     * Date 09/08/2022
-     * @param employee
-     * @return  true: edit employee success, status 200 / false: status 404
-     * if employee null : Create new employee
-     * @return  update Employee success
-     */
-    Employee editEmployee(Employee employee);
 
 
 
