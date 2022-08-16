06a8528ea1d2eeeec85958984b1cefd9cac83dd0
Binh96 <phanxuanbinh96@gmail.com>
2022-08-17 06:26:29 +0700

    BinhPX remove authorization of method getAllDishType

--- coffee-store/src/main/java/com/coffee/controller/DishTypeRestController.java
+++ coffee-store/src/main/java/com/coffee/controller/DishTypeRestController.java
@@ -33,7 +33,7 @@ public class DishTypeRestController {
      * @return HTTP status  200(OK) : return Page<DishType> dishTypePage
      * HTTP status  204(NO_CONTENT): return dishTypePage is empty
      */
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+//    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/getDishTypePage")
     public ResponseEntity<Page<DishType>> getAllDishType(@PageableDefault(10) Pageable pageable) {
         Page<DishType> dishTypePage = this.iDishTypeService.findAllDishType(pageable);
