8a37b35860d09a85f6b3bdf14d3f50c306f4a14d
C0222G2_LeVanTai <levantai1496@gmail.com>
2022-09-03 15:17:52 +0700

    tailv add new source code employee

--- coffee-store/src/main/java/com/coffee/controller/EmployeeRestController.java
+++ coffee-store/src/main/java/com/coffee/controller/EmployeeRestController.java
@@ -49,7 +49,7 @@ public class EmployeeRestController {
      * @param searchAccount
      * @return
      */
-//    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/employee/page")
     public ResponseEntity<Page<IEmployeeDTO>> getAllEmployee(@PageableDefault(10) Pageable pageable,
                                                              Optional<String> searchName,
